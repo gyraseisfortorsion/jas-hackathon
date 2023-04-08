b5c326754d1e49030186b2e8774bf106fcdde44f
Arman Bolatov <a.bol@bc-pf.org>
2023-04-09 00:06:13 +0600

    changed streamlit app

--- website/streamlit/main.py
+++ website/streamlit/main.py
@@ -1,40 +1,119 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import altair as alt
+from vega_datasets import data
+
 
 st.backround_color = 'white'
-map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
+st.set_page_config(
+    page_title="Time series annotations", page_icon="⬇", layout="centered"
+)
 
-st.map(map_data)
 
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
+@st.experimental_memo
+def get_data():
+    source = data.stocks()
+    source = source[source.date.gt("2004-01-01")]
+    return source
 
-st.line_chart(chart_data)
 
+@st.experimental_memo(ttl=60 * 60 * 24)
+def get_chart(data):
+    hover = alt.selection_single(
+        fields=["date"],
+        nearest=True,
+        on="mouseover",
+        empty="none",
+    )
+
+    lines = (
+        alt.Chart(data, height=500, title="Evolution of stock prices")
+        .mark_line()
+        .encode(
+            x=alt.X("date", title="Date"),
+            y=alt.Y("price", title="Price"),
+            color="symbol",
+        )
+    )
+
+    # Draw points on the line, and highlight based on selection
+    points = lines.transform_filter(hover).mark_circle(size=65)
+
+    # Draw a rule at the location of the selection
+    tooltips = (
+        alt.Chart(data)
+        .mark_rule()
+        .encode(
+            x="yearmonthdate(date)",
+            y="price",
+            opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
+            tooltip=[
+                alt.Tooltip("date", title="Date & Time"),
+                alt.Tooltip("price", title="Number of customers in a sector"),
+            ],
+        )
+        .add_selection(hover)
+    )
+
+    return (lines + points + tooltips).interactive()
+
+
+st.title("⬇ Time series annotations")
+
+
+col1, col2 = st.columns(2)
+ticker = "💬"
+ticker_dx = 0
+ticker_dy = -10
+
+# Original time series chart. Omitted `get_chart` for clarity
+source = get_data()
+chart = get_chart(source)
+
+# Input annotations
+ANNOTATIONS = [
+    ("Mar 01, 2008", "Pretty good day for GOOG"),
+    ("Dec 01, 2007", "Something's going wrong for GOOG & AAPL"),
+    ("Nov 01, 2008", "Market starts again thanks to..."),
+    ("Dec 01, 2009", "Small crash for GOOG after..."),
+]
+
+# Create a chart with annotations
+annotations_df = pd.DataFrame(ANNOTATIONS, columns=["date", "event"])
+annotations_df.date = pd.to_datetime(annotations_df.date)
+annotations_df["y"] = 0
+annotation_layer = (
+    alt.Chart(annotations_df)
+    .mark_text(size=15, text=ticker, dx=ticker_dx, dy=ticker_dy, align="center")
+    .encode(
+        x="date:T",
+        y=alt.Y("y:Q"),
+        tooltip=["event"],
+    )
+    .interactive()
+)
+
+# Display both charts together
+st.altair_chart((chart + annotation_layer).interactive(), use_container_width=True)
+
+# Hide footer
 hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            footer {
-	
-	visibility: hidden;
-	
-	}
-footer:after {
-	content:''; 
-	visibility: visible;
-	display: block;
-	position: relative;
-	#background-color: red;
-	padding: 5px;
-	top: 2px;
-}
-            </style>
-            """
+<style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    footer {
+        visibility: hidden;
+    }
+    footer:after {
+        content:''; 
+        visibility: visible;
+        display: block;
+        position: relative;
+        #background-color: red;
+        padding: 5px;
+        top: 2px;
+    }
+</style>
+"""
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
-
-
