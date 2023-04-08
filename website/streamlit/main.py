e98792988e6f9b68e5c4344815b84f74cadcd0d2
askarali <56202525+gyraseisfortorsion@users.noreply.github.com>
2023-04-09 04:56:08 +0600

    authentification

--- website/streamlit/main.py
+++ website/streamlit/main.py
@@ -6,117 +6,160 @@ from prophet.diagnostics import performance_metrics
 from prophet.diagnostics import cross_validation
 from prophet.plot import plot_cross_validation_metric
 import base64
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+import pickle
+from pathlib import Path
 
-
-#link rel css stylesheet
-
+st.title("Main")
 with open('style.css') as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+# --- USER AUTHENTICATION ---
+names = ["Peter Parker", "Rebecca Miller"]
+usernames = ["pparker", "rmiller"]
 
+# load hashed passwords
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+hashed_passwords = stauth.Hasher(['abc', 'def']).generate()
+for i in hashed_passwords:
+    print(i+"\n\n")
+    
+# with file_path.open("rb") as file:
+#     hashed_passwords = pickle.load(file)
 
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
 
-
-col1,col2= st.columns([2,1])
-
-
-with col1:
-    with st.container():
-        col3,col4 = st.columns(2)
-        with col3:
-            st.image("https://pbs.twimg.com/profile_images/1544722618275827713/9-aMN_Wb_400x400.jpg")
-        with col4:
-            data = np.random.randn(20,1)
-            st.bar_chart(data)
-
-    df = pd.DataFrame(
-    np.random.randn(10, 5),
-    columns=('col %d' % i for i in range(5)))
-    #st.table(df)
-    ndata = np.random.randn(100,10)
-    ndata = abs(ndata)
-    st.bar_chart(ndata)
-
-
-
-with col2:
-    with st.container():
-        st.subheader("Hello")
-
-#############
-
-
-# add sidebar menu
-st.sidebar.title('📈 Прогнозирование активности')
-
-st.title('📈 Cистема прогнозирования покупательской активности')
-
-"""
-### Инструкция по использованию
-В данной вкладке вы можете видеть предсказывать временные ряды покупок за каждый блок и скачать его в формате 'csv'. Для этого выберите горизонт предсказания. Имейте ввиду что точность прогноза уменьшается с увеличением горизонта прогноза. Также вы можете выбрать сектор, для которого хотите построить прогноз. После нажмите на кнопку 'Прогнозировать'.
-"""
-
-periods_input = st.number_input(
-    'Горизонт предсказания (в днях)',
-    min_value = 1, max_value = 365
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
 )
 
-# select sector
-sector = st.selectbox(
-    'Выберите сектор',
-    ('Сектор 1', 'Сектор 2', 'Сектор 3', 'Сектор 4', 'Сектор 5', 'Сектор 6',)
-)
-sector_number = sector.split(' ')[1]
+name, authentication_status, username = authenticator.login("Login", "main")
 
-if sector:
-    data = pd.read_csv(f'data/sector{sector_number}.csv')
-    data['ds'] = pd.to_datetime(data['ds'],errors='coerce')
+if authentication_status == False:
+    st.error("Username/password is incorrect")
 
-    # rename columns
-    data_presentation = data.rename(columns={'ds': 'Дата', 'y': 'Количество людей'})
-    st.dataframe(data_presentation, use_container_width=True)
+if authentication_status == None:
+    st.warning("Please enter your username and password")
 
-    max_date = data['ds'].max()
+if authentication_status:
+    authenticator.logout("Logout", "sidebar")
+    st.write(f'Welcome *{name}*')
+    st.sidebar.title(f"Welcome {name}")
+    #link rel css stylesheet
 
-    if st.button('Прогнозировать'):
-
-        m = Prophet()
-        m.fit(data)
-
-        """
-        ### Визуализация прогноза
-        Графики ниже показывают прогнозируемые значения. "yhat" - это прогнозируемое значение, а верхняя и нижняя границы - это (по умолчанию) доверительные интервалы в 80%.
-        """
-
-        future = m.make_future_dataframe(periods=periods_input)
-        
-        forecast = m.predict(future)
-        fcst = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
-
-        fcst_filtered =  fcst[fcst['ds'] > max_date]
-        fcst_filtered = fcst_filtered.rename(columns={
-            'ds': 'Дата', 'yhat': 'Количество людей', 'yhat_lower': 'Нижняя граница', 'yhat_upper': 'Верхняя граница'
-        })
-        st.dataframe(fcst_filtered, use_container_width=True)
-        
-        """
-        Следующая визуализация показывает прогнозируемые значения (синие точки) и истинные значения (черные точки).
-        """
-        fig1 = m.plot(forecast)
-        st.write(fig1)
-
-        """
-        Следующие несколько визуализаций показывают тенденции прогнозируемых значений, тенденции дня недели и годовые тенденции (если набор данных содержит несколько лет). Синяя затененная область представляет верхние и нижние доверительные интервалы.
-        """
-        fig2 = m.plot_components(forecast)
-        st.write(fig2)
+    
 
 
-        """
-        ### Скачать прогноз
-        По ссылке ниже вы можете скачать прогноз в формате 'csv'. После скачивания файла, вы можете открыть его в Excel и проанализировать.
-        """
-        csv_exp = fcst_filtered.to_csv(index=False)
-        # When no file name is given, pandas returns the CSV as a string, nice.
-        b64 = base64.b64encode(csv_exp.encode()).decode()  # some strings <-> bytes conversions necessary here
-        href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a>'
-        st.markdown(href, unsafe_allow_html=True)
+
+
+    col1,col2= st.columns([2,1])
+
+
+    with col1:
+        with st.container():
+            col3,col4 = st.columns(2)
+            with col3:
+                st.image("https://pbs.twimg.com/profile_images/1544722618275827713/9-aMN_Wb_400x400.jpg")
+            with col4:
+                data = np.random.randn(20,1)
+                st.bar_chart(data)
+
+        df = pd.DataFrame(
+        np.random.randn(10, 5),
+        columns=('col %d' % i for i in range(5)))
+        #st.table(df)
+        ndata = np.random.randn(100,10)
+        ndata = abs(ndata)
+        st.bar_chart(ndata)
+
+
+
+    with col2:
+        with st.container():
+            st.subheader("Hello")
+
+    #############
+
+
+
+    # add sidebar menu
+    # st.sidebar.title('📈 Прогнозирование активности')
+
+    st.title('📈 Cистема прогнозирования покупательской активности')
+
+    """
+    ### Инструкция по использованию
+    В данной вкладке вы можете видеть предсказывать временные ряды покупок за каждый блок и скачать его в формате 'csv'. Для этого выберите горизонт предсказания. Имейте ввиду что точность прогноза уменьшается с увеличением горизонта прогноза. Также вы можете выбрать сектор, для которого хотите построить прогноз. После нажмите на кнопку 'Прогнозировать'.
+    """
+
+    periods_input = st.number_input(
+        'Горизонт предсказания (в днях)',
+        min_value = 1, max_value = 365
+    )
+
+    # select sector
+    sector = st.selectbox(
+        'Выберите сектор',
+        ('Сектор 1', 'Сектор 2', 'Сектор 3', 'Сектор 4', 'Сектор 5', 'Сектор 6',)
+    )
+    sector_number = sector.split(' ')[1]
+
+    if sector:
+        data = pd.read_csv(f'data/sector{sector_number}.csv')
+        data['ds'] = pd.to_datetime(data['ds'],errors='coerce')
+
+        # rename columns
+        data_presentation = data.rename(columns={'ds': 'Дата', 'y': 'Количество людей'})
+        st.dataframe(data_presentation, use_container_width=True)
+
+        max_date = data['ds'].max()
+
+        if st.button('Прогнозировать'):
+
+            m = Prophet()
+            m.fit(data)
+
+            """
+            ### Визуализация прогноза
+            Графики ниже показывают прогнозируемые значения. "yhat" - это прогнозируемое значение, а верхняя и нижняя границы - это (по умолчанию) доверительные интервалы в 80%.
+            """
+
+            future = m.make_future_dataframe(periods=periods_input)
+            
+            forecast = m.predict(future)
+            fcst = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
+
+            fcst_filtered =  fcst[fcst['ds'] > max_date]
+            fcst_filtered = fcst_filtered.rename(columns={
+                'ds': 'Дата', 'yhat': 'Количество людей', 'yhat_lower': 'Нижняя граница', 'yhat_upper': 'Верхняя граница'
+            })
+            st.dataframe(fcst_filtered, use_container_width=True)
+            
+            """
+            Следующая визуализация показывает прогнозируемые значения (синие точки) и истинные значения (черные точки).
+            """
+            fig1 = m.plot(forecast)
+            st.write(fig1)
+
+            """
+            Следующие несколько визуализаций показывают тенденции прогнозируемых значений, тенденции дня недели и годовые тенденции (если набор данных содержит несколько лет). Синяя затененная область представляет верхние и нижние доверительные интервалы.
+            """
+            fig2 = m.plot_components(forecast)
+            st.write(fig2)
+
+
+            """
+            ### Скачать прогноз
+            По ссылке ниже вы можете скачать прогноз в формате 'csv'. После скачивания файла, вы можете открыть его в Excel и проанализировать.
+            """
+            csv_exp = fcst_filtered.to_csv(index=False)
+            # When no file name is given, pandas returns the CSV as a string, nice.
+            b64 = base64.b64encode(csv_exp.encode()).decode()  # some strings <-> bytes conversions necessary here
+            href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a>'
+            st.markdown(href, unsafe_allow_html=True)
